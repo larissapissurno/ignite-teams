a6f6124fa0691a1a532ad25346305efd6047e248
Larissa Pissurno <larissapissurno@gmail.com>
2023-08-05 21:37:15 -0300

    initial structure

---
+++ src/screens/groups/Groups.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import * as S from "./Groups.styles";
+
+export const Groups = () => {
+  return (
+    <S.Container>
+      <S.Title>Groups</S.Title>
+    </S.Container>
+  );
+};
