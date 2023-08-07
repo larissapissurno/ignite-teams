e65e02ac5ccb17df3e3a1f4fca14645774356561
Larissa Pissurno <larissapissurno@gmail.com>
2023-08-07 12:30:41 -0300

    improvements on loading indicator

--- src/screens/groups/Groups.tsx
+++ src/screens/groups/Groups.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { View, Text } from "react-native";
 import * as S from "./Groups.styles";
 
-export const Groups = () => {
+export function Groups() {
   return (
     <S.Container>
       <S.Title>Groups</S.Title>
     </S.Container>
   );
-};
+}
