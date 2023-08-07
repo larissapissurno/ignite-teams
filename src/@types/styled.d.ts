727d796a3cafcd41900f032b6df074de12ba9977
Larissa Pissurno <larissapissurno@gmail.com>
2023-08-07 12:19:31 -0300

    integration with theme

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components/native'
+import defaultTheme from '@themes/default'
+
+
+declare module 'styled-components/native' {
+  type ThemeTpe = typeof defaultTheme;
+
+  export interface DefaultTheme extends ThemeTpe { }
+}
--- src/screens/groups/Groups.styles.ts
+++ src/screens/groups/Groups.styles.ts
@@ -1,10 +1,13 @@
+import { DefaultTheme } from "styled-components";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #000;
+  background-color: ${(props) => props.theme.COLORS.GRAY_600};
   align-items: center;
   justify-content: center;
+
+  padding: 24px;
 `;
 
 export const Title = styled.Text`
