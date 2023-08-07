8fbaaa12a860b8e01e5dbba76701fe4490c6bc30
Larissa Pissurno <larissapissurno@gmail.com>
2023-08-07 12:33:20 -0300

    status bar stylings

--- App.tsx
+++ App.tsx
@@ -8,12 +8,19 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/loading/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
