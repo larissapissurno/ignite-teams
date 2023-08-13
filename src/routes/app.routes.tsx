f4a277571c35a85f40e877772318391d5f833aec
Larissa Pissurno <larissapissurno@gmail.com>
2023-08-13 18:15:41 -0300

    configure routes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Groups } from "@screens/groups/Groups";
+import { NewGroup } from "@screens/new-group/NewGroup";
+import { Players } from "@screens/players/Players";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator>
+      <Screen name="groups" component={Groups} />
+      <Screen name="new" component={NewGroup} />
+      <Screen name="players" component={Players} />
+    </Navigator>
+  );
+}
