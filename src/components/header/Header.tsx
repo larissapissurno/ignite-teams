3f55d26065f37fd42d685d7987439d49533fc936
Larissa Pissurno <larissapissurno@gmail.com>
2023-08-13 18:38:23 -0300

    implement goback

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,23 @@
 import { Text } from "react-native";
 import { BackButton, BackIcon, Container, Logo } from "./Header.styles";
 import logoImg from "@assets/logo.png";
+import { useNavigation } from "@react-navigation/native";
 
 type HeaderProps = {
   showBackButton?: boolean;
 };
 
 export function Header({ showBackButton = false }: HeaderProps) {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <Container>
       {showBackButton && (
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       )}
